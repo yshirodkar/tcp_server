0922f87134e88b4d83b59692a3a5af4bd6d224c9
yogesh <yogesh2490@gmail.com>
2019-02-28 23:16:09 -0700

    removing unnecessary code

--- models/Response.go
+++
@@ -1,8 +0,0 @@
-package models
-
-const RESPONSE_SERVER_ERROR = "internal_server_error"
-const RESPONSE_INVALID_PAYLOAD = "invalid_payload"
-const RESPONSE_BAD_DATA_PROVIDED = "bad_data_provided"
-const RESPONSE_DATA_NOT_FOUND = "data_not_found"
-const CRUCIAL_DATA_NOT_PROVIDED = "crucial_data_not_provided"
-const MISSING_DECODED_JWT_TOKEN_DATA = "missing decoded jwt token data"
